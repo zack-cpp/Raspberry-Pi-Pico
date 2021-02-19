0791262d102bcee8e17b12b3cbfe8b02f3643ec6
zack-cpp <neojoyko@gmail.com>
2021-02-20 06:51:28 +0700

    inital commit

---
+++ Libraries/i2c_lcd/examples/main.py
@@ -0,0 +1,11 @@
+from pico_i2c_lcd import I2cLcd
+from machine import I2C
+from machine import Pin
+import utime as time
+
+i2c = I2C(id=1,scl=Pin(27),sda=Pin(26),freq=100000)
+lcd = I2cLcd(i2c, 0x27, 2, 16) # LCD 16x2
+
+while True:
+    lcd.move_to(0,0)
+    lcd.putstr('Hello World')
